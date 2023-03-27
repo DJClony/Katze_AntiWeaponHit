d0f578febbee9aa4a6a1de51faeebded183cd16a
DJClony <108025053+DJClony@users.noreply.github.com>
2023-03-28 00:28:30 +0200

    Add files via upload

---
+++ katze_weapon/client/cl_katze.lua
@@ -0,0 +1,21 @@
+print('[KATZE]: Script geladen')
+
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+		local ped = GetPlayerPed( -1 )
+		local weapon = GetSelectedPedWeapon(ped)
+		if IsPedArmed(ped, 6) then
+        	DisableControlAction(1, 140, true)
+            DisableControlAction(1, 141, true)
+            DisableControlAction(1, 142, true)
+        end
+    end
+end)
+
+function ShowNotification(text)
+	SetNotificationTextEntry("STRING")
+	AddTextComponentString(text)
+	DrawNotification(false, false)
+end
---
+++ katze_weapon/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Katze'
+description 'Deaktiviert den Waffenschlag'
+version '1.0.1'
+
+client_script 'client/cl_katze.lua'
